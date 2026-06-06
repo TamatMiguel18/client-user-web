1d56285747bbc914c910f1573907801788a7fa6c
TamatMiguel18 <mtamat-2024024@kinal.edu.gt>
2026-06-06 12:41:17 -0600

    Add src/app/router/AppRouter.jsx

---
+++ client-user/src/app/router/AppRouter.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { UserLayout } from '../layout/UserLayout';
+import { CropsList } from '../../features/crops/components/CropsList';
+import { FertilizersList } from '../../features/fertilizers/components/FertilizersList';
+import { FieldsManager } from '../../features/fields/components/FieldsManager';
+import { FertilityForm } from '../../features/fertility/components/FertilityForm';
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<UserLayout />}>
+        {/* Default route redirects to crops */}
+        <Route index element={<Navigate to="/crops" replace />} />
+        
+        <Route path="crops" element={<CropsList />} />
+        <Route path="fertilizers" element={<FertilizersList />} />
+        <Route path="fields" element={<FieldsManager />} />
+        <Route path="recommendations" element={<FertilityForm />} />
+        
+        {/* Catch all route */}
+        <Route path="*" element={<Navigate to="/crops" replace />} />
+      </Route>
+    </Routes>
+  );
+};
