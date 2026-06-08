cac092614b493f35aafe96fce874b989132424ca
azacarias-dev <azacarias-2024007@kinal.edu.gt>
2026-06-08 08:41:59 -0600

    Se creo el un archivo para las rutas del usuario

--- client-user/src/app/router/AppRouter.jsx
+++ client-user/src/app/router/AppRouter.jsx
@@ -2,14 +2,29 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { UserLayout } from '../layout/UserLayout';
 import { Dashboard } from '../../features/dashboard/components/Dashboard';
+import { AuthPage } from '../../features/auth/pages/AuthPage';
+import { useAuthStore } from '../../features/auth/store/authStore';
 
 export const AppRouter = () => {
+  const isAuthenticated = useAuthStore(state => state.isAuthenticated);
+
   return (
     <Routes>
-      <Route path="/" element={<UserLayout />}>
-        <Route index element={<Navigate to="/dashboard" replace />} />
-        <Route path="dashboard" element={<Dashboard />} />
+      {/* PUBLIC */}
+      <Route path="/" element={
+          isAuthenticated ? <Navigate to="/dashboard" replace /> : <AuthPage />
+      } />
+      <Route path="/reset-password" element={<AuthPage />} />
+
+      {/* DASHBOARD (PROTECTED) */}
+      <Route path="/dashboard" element={
+          isAuthenticated ? <UserLayout /> : <Navigate to="/" replace />
+      }>
+        <Route index element={<Dashboard />} />
       </Route>
+      
+      {/* CATCH ALL */}
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 };
