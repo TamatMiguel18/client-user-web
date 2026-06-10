3adc52984314a021bda9d281604e93db15270d42
azacarias-dev <azacarias-2024007@kinal.edu.gt>
2026-06-09 19:41:26 -0600

    Creacion de .env de ejemplo

--- client-user/src/features/crops/api/crops.api.js
+++
@@ -1,9 +0,0 @@
-import api from '../../../shared/api/api';
-
-export const getCrops = async () => {
-  return await api.get('/cultivos/');
-};
-
-export const getCropByName = async (nombreCultivo) => {
-  return await api.get(`/cultivos/${encodeURIComponent(nombreCultivo)}`);
-};
--- client-user/src/features/fertility/api/fertility.api.js
+++
@@ -1,5 +0,0 @@
-import api from '../../../shared/api/api';
-
-export const calculateRecommendation = async (data) => {
-  return await api.post('/fertility/calculate', data);
-};
